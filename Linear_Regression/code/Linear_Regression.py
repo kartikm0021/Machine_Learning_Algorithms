9c2c77b6c49c27dcd53a5d2fc370657329022947
LAPTOP-D6AAIMDO\User <kartik.nus@gmail.com>
2021-02-22 12:30:37 +0800

    commit

--- Linear_Regression/code/Linear_Regression.py
+++ Linear_Regression/code/Linear_Regression.py
@@ -3,11 +3,16 @@ import csv
 import time
 from sklearn.metrics import roc_curve
 from matplotlib import pyplot
+# np.seterr(divide='ignore', invalid='ignore')
 
 
 class Linear_Regression:
-    def __init__(self, X, y, alpha=0.03, iterations=1500):
+    def __init__(self, X, y, alpha=0.01, iterations=200000):
 
+        self.train_percentages = [0.7]
+        self.cross_fold_values = [15]
+        self.learning_rates = [.001]
+        self.statistics = []
         self.alpha = alpha
         self.iterations = iterations
         self.total_number_of_records = len(y)
@@ -66,6 +71,7 @@ class Linear_Regression:
             X = self.X
         else:
             total_number_of_records = np.size(X, 0)
+
             X = np.hstack((np.ones(
                 (total_number_of_records, 1)), (X - np.mean(X, 0)) / np.std(X, 0)))
 
@@ -108,6 +114,114 @@ class Linear_Regression:
         sample = int(percentTrain*N)
         x_train, x_test = X[:sample, :], X[sample:, :]
         return [x_train, x_test]
+
+    @classmethod
+    def splitCV(cls, X, folds):
+        """
+        Takes in the normalized dataset X_norm ,and the number of folds needed
+        This would split the number of records equlilantly in every partition. If k is a number which cannot be equliably splitted
+        it would distribute the extra records into all the partitions.
+        :X: nd array object normalized.
+        :folds: number of folds needed.
+        :return: list of numpy array objects containing the different folds or partitions
+        """
+        np.random.shuffle(X)
+        split_array = np.array_split(X, folds)
+        return split_array
+
+    def trainandTest(self, X_Train, X_Test, learning_rate):
+        """
+        Takes in the normalized dataset X_train,X_test and learning rate
+        :X_train: train data set.
+        :X_test: test data set.
+        :learning_rate: Learning Rate.
+        :return:  prediction values, weights, accuracy score
+        """
+
+        X = X_Train[:, :-1]
+        Y = X_Train[:, -1]
+        regressor = Linear_Regression(X, Y, alpha=learning_rate).model()
+        train_accuracy = regressor.accuracy()
+        # print(train_accuracy)
+
+        test_X = X_Test[:, :-1]
+        test_Y = X_Test[:, -1]
+
+        test_accuracy_score = regressor.accuracy(test_X, test_Y)
+        # print(test_accuracy_score)
+
+        y_prediction_value = regressor.predict(X)
+        weights = regressor.get_weights()
+
+        return y_prediction_value, weights, test_accuracy_score
+
+    def k_fold_cross_validation(self, X, folds, learning_rate):
+        """
+        Takes in the Normalized array and number of k-values needed and the number of folds needed.
+        The function would iterate over all the fold partitions except the fold in enumeration
+        and get the other folds and call the Linear regression algorithm those many times to get the accuracy.
+        The returned accuracy is the mean of the individual fold accuracies and also a list of predicted labels
+        which would be used in the Classification report
+        :X: Train Data set which is a nd array object normalized.
+        :k: k-value.
+        :folds: number of folds for which knn needs to be done.
+        :return: accuracy of this iteration and list of predicted outputs
+        """
+        weights_accuracy_vector = []
+        accuracy_listing = []
+        k_fold_partitions = self.splitCV(X, folds)
+        for index, item in enumerate(k_fold_partitions):
+            cross_validation_dataset = item
+            list_of_items_from_zero_to_index = k_fold_partitions[0:index]
+            list_of_items_from_index_to_end = k_fold_partitions[index+1:]
+            total_train_list = list_of_items_from_zero_to_index + \
+                list_of_items_from_index_to_end
+            train_data_set = np.vstack(total_train_list)
+            y_prediction_value, theta, accuracy_score = self.trainandTest(
+                train_data_set, cross_validation_dataset, learning_rate)
+            # print(f'Thetha is : {theta}')
+            weights_accuracy_vector.append(
+                (index, accuracy_score, theta, learning_rate, cross_validation_dataset, y_prediction_value))
+            if accuracy_score is not np.nan:
+                accuracy_listing.append(accuracy_score)
+        print(f'Accuracy Listing {accuracy_listing}')
+        accuracy_average = np.average(accuracy_listing)
+
+        print(
+            f'Folds : {folds}, Accuracy Average : {accuracy_average} ')
+        return accuracy_average, weights_accuracy_vector
+
+    def trigger_k_fold_cross_validation(self, X):
+        """
+        Trigger the train and given the input data set 
+        :X: Train Data set which is a nd array object normalized.
+        """
+        for train_percentage in self.train_percentages:
+            print('*'*20)
+            print(f' Training with {train_percentage*100} % data')
+            print('-'*10)
+            x_train, x_test = Linear_Regression.splitTT(X, train_percentage)
+            accuracy_listing = []
+            for cross_fold in self.cross_fold_values:
+                for learning_rate in self.learning_rates:
+                    tic = time.perf_counter()
+                    accuracy_cross_fold, weights_accuracy_vector = self.k_fold_cross_validation(
+                        x_train, cross_fold, learning_rate)
+                    toc = time.perf_counter()
+                    time_taken = toc - tic
+                    accuracy_listing.append(accuracy_cross_fold)
+                    datapoint = ('Linear Regression', train_percentage *
+                                 100, learning_rate, cross_fold, accuracy_cross_fold, time_taken, weights_accuracy_vector)
+                    self.statistics.append((datapoint))
+
+            average_accuracy_score = np.mean(accuracy_listing)
+            print(
+                f' Average accuracy for all cross folds : {average_accuracy_score:.3f}')
+            print('Mean Accuracy: ' +
+                  "{:.2f}".format(average_accuracy_score*100)+'%')
+        print('Statistics')
+        # print(self.statistics)
+        return self.statistics
 
 
 # https://towardsdatascience.com/linear-regression-from-scratch-with-numpy-implementation-finally-8e617d8e274c
--- Linear_Regression/code/driver_sklearn.py
+++ Linear_Regression/code/driver_sklearn.py
@@ -11,6 +11,9 @@ data = data = np.genfromtxt(
     data_file_name)
 # data = classifier.dataStandardization(data)
 
+data = np.delete(data, [3, 1], 1)
+
+print(data[0:])
 x_train, x_test = classifier.splitTT(data, .8)
 
 print(x_train.shape)
@@ -40,3 +43,6 @@ print(our_test_accuracy)
 
 
 # our_test_accuracy = our_regressor.score(X_test, y_test)
+
+
+statistics = classifier(X, Y).trigger_k_fold_cross_validation(data)
